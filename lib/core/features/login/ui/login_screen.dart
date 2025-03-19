0267ba47f7006dc8894276a1e28338cc6550732e
abood-sh <abood_shaban_1999@hotmail.com>
2025-03-19 15:16:48 +0200

    .SignUp Screen (ui - cubit - repo - model - add route & router - di for signUp - phone regex)  & .in folder widget add already have account and navigation - add bloc listener. .edit in login screen don't have account and navigator.

--- lib/core/features/login/ui/login_screen.dart
+++ lib/core/features/login/ui/login_screen.dart
@@ -75,9 +75,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   void validateThenLogin(BuildContext context) {
     if (context.read<LoginCubit>().formKey.currentState!.validate()) {
-      context.read<LoginCubit>().emitLoginState(LoginReqBody(
-          email: context.read<LoginCubit>().emailController.text,
-          password: context.read<LoginCubit>().passwordController.text));
+      context.read<LoginCubit>().emitLoginStates();
     }
   }
 }
--- lib/core/routing/routers.dart
+++ lib/core/routing/routers.dart
@@ -1,6 +1,6 @@
 class Routers {
   static const String onboardingScreen = '/onboardingScreen';
   static const String loginScreen = '/login';
-  static const String registerScreen = '/register';
+  static const String signupScreen = '/signupScreen';
   static const String homeScreen = '/homeScreen';
 }
